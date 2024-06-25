dd2f1222539245b1867b06c50842d1b2a85ad271
Alan Fayz <alanfayz123@gmail.com>
2024-06-25 16:08:46 +0100

    final final finish

--- PushUpdates.py
+++ PushUpdates.py
@@ -1,6 +1,6 @@
 import subprocess
 
-commit_description = '"' + input("commit description: ") + '"'
+commit_description = input("commit description: ")
 
 subprocess.run(["git", "add", "."])
 subprocess.run(["git", "commit", "-m", commit_description])
