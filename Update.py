87144f3dfc5df7c18d6b32a81b8b63d5198e9293
Alan Fayz <alanfayz123@gmail.com>
2024-06-25 16:03:15 +0100

    "commit program test"

---
+++ Update.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+commit_description = '"' + input("commit description: ") + '"'
+
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", commit_description])
+subprocess.run(["git", "push"])
+
+x = input()
