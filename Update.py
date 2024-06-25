39aa06be067a68da6ab1fd7c3db1492dd1050bfe
Alan Fayz <alanfayz123@gmail.com>
2024-06-25 16:07:51 +0100

    "test"

--- Update.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-commit_description = input("commit description: ")
-
-subprocess.run(["git", "add", "."])
-subprocess.run(["git", "commit", "-m", commit_description])
-subprocess.run(["git", "push"])
-
-x = input("press enter to finish")
